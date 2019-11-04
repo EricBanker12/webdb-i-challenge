c59689b704600b034ff51e4b38f619db088e61d2
ericbanker12 <ericbanker12@gmail.com>
2019-11-04 14:55:43 -0600

    add GET for account by id

--- server.js
+++ server.js
@@ -18,4 +18,17 @@ server.get('/api/accounts', (req, res) => {
     })
 })
 
+server.get('/api/accounts/:id', (req, res) => {
+    db('accounts').where({id: req.params.id}).first()
+    .then(resp => {
+        // console.log(resp)
+        if (resp) res.json(resp)
+        else res.status(404).json({message: `No account of id ${req.params.id} was found.`})
+    })
+    .catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+    })
+})
+
 module.exports = server;
