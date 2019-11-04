faf19c092f24c011dca53172214a87041102c9a7
ericbanker12 <ericbanker12@gmail.com>
2019-11-04 14:50:35 -0600

    add GET for array of all accounts

--- server.js
+++ server.js
@@ -6,4 +6,16 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db('accounts')
+    .then(resp => {
+        // console.log(resp)
+        res.json(resp)
+    })
+    .catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+    })
+})
+
 module.exports = server;
